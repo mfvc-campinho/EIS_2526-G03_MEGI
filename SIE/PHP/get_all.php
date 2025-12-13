7c73833ce944dfc3cb5a0d5c5ee4e622c4d32217
FM11pp3 <133037386+FM11pp3@users.noreply.github.com>
2025-12-13 12:00:07 +0000

    delete PHP

--- SIE/PHP/get_all.php
+++
@@ -1,9 +0,0 @@
-<?php
-header('Content-Type: application/json; charset=utf-8');
-require_once __DIR__ . '/includes/data_loader.php';
-
-$out = load_app_data($mysqli);
-
-echo json_encode($out, JSON_UNESCAPED_UNICODE);
-
-$mysqli->close();
